72f20477b045822cc5ad3c261a785d3422622bfd
Chris Boyle <christopher.boyle.101@strath.ac.uk>
2020-02-29 14:06:45 +0000

    Update anim every frame

--- src/sr_anim.cpp
+++ src/sr_anim.cpp
@@ -1,15 +1,11 @@
 #include "simrenderer.hpp"
 
-void Renderer::animate()
+bool Renderer::frameEnded(const Ogre::FrameEvent &evt)
 {
+  this->time += evt.timeSinceLastFrame;
   auto scene_manager = this->getRoot()->getSceneManager("mainSceneManager");
-  auto anim = scene_manager->createAnimation("anim", 100.0);
-
-  // create tracks for nodes
-  unsigned short handles = 0;
-  for (auto point: this->point_nodes) {
-    auto track = anim->createNodeTrack(handle++);
-  }
-
-  // apply
+  auto anim = scene_manager->getAnimation("anim");
+  anim->apply(this->time);
+  std::cerr << this->time << std::endl;
+  return true;
 }

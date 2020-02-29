cbec0bc420dd5cbef65eab7c9dc8e2a23338bbd6
Chris Boyle <christopher.boyle.101@strath.ac.uk>
2020-02-29 14:08:34 +0000

    Reduced initial camera distance

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,7 @@ int main(void) //int argc, char **argv)
 {
   try
   {
-  	Renderer app(1000, 0.01, 10.0, 10.0);
+  	Renderer app(10, 0.01, 10.0, 10.0);
     app.initApp();
     //app.readTrajectory(); // TODO
     app.getRoot()->startRendering();

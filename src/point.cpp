995d16382fc24471a46ce469beaf615306d0890e
Chris Boyle <christopher.boyle.101@strath.ac.uk>
2020-02-29 14:07:28 +0000

    Gave point the control over its animation;
    added func to build anim from points

--- src/point.cpp
+++ src/point.cpp
@@ -1,17 +1,55 @@
+#include <iostream>
+
 #include "point.hpp"
 
-Point::Point()
+static unsigned short handlec = 0;
+
+Point::Point(OgreBites::ApplicationContext *ctxt, Ogre::Vector3 position, double radius)
 {
-  this->r = 1.0;
+  this->positions.push_back(position);
+  this->times.push_back(0.0);
+  this->r = radius;
+  this->ctxt = ctxt;
+
+  auto scene_manager = this->ctxt->getRoot()->getSceneManager("mainSceneManager");
+  Ogre::Entity* ballEntity = scene_manager->createEntity("sphere.mesh"); // default diameter 100 units
+  this->node = scene_manager->getRootSceneNode()->createChildSceneNode();
+  this->node->attachObject(ballEntity);
+  this->node->setPosition(position);
+  this->node->setScale(r/200.0, r/200.0, r/200.0);
+  std::cerr << this->node->getPosition() << r/200.0 << std::endl;
 }
 
-Point::Point(double x, double y, double z, double r)
+void Point::add_timepoint(Ogre::Vector3 position, double time)
 {
-  this->positions.push_back(Ogre::Vector3(x, y, z));
-  this->r = r;
+  this->positions.push_back(position);
+  this->times.push_back(time);
 }
 
-Ogre::Vector3 get_position(int timestep)
+Ogre::Vector3 Point::get_position(int timestep) const
 {
   return this->positions[timestep];
+}
+
+double Point::get_time(int timestep) const
+{
+  return this->times[timestep];
+}
+
+Ogre::SceneNode *Point::get_node() const
+{
+  return this->node;
+}
+
+void Point::generate_anim() 
+{
+  auto scene_manager = this->ctxt->getRoot()->getSceneManager("mainSceneManager");
+  auto anim = scene_manager->getAnimation("anim");
+  auto track = anim->createNodeTrack(handlec++, this->node);
+  for (unsigned int i = 1; i < this->positions.size(); i++) {
+    auto time = this->get_time(i);
+    auto dposition = this->get_position(i) - this->get_position(i-1);;
+    auto key_frame = track->createNodeKeyFrame(time);
+    key_frame->setTranslate(dposition);
+  }
 }
--- src/point.hpp
+++ src/point.hpp
@@ -1,18 +1,26 @@
 #pragma once
 #include <vector>
+
 #include <Ogre.h>
+#include "OgreApplicationContext.h"
 
 class Point {
   private:
     std::vector<Ogre::Vector3> positions;
+    std::vector<double> times;
     double r;
+    Ogre::SceneNode *node;
+    OgreBites::ApplicationContext *ctxt;
 
 
   public:
-    Point();
-    Point(double x, double y, double z, double r);
-    void add_timepoint(double x, double y, double z);
+    Point(OgreBites::ApplicationContext *ctxt, Ogre::Vector3 position, double radius);
+    void add_timepoint(Ogre::Vector3 position, double time);
 
     Ogre::Vector3 get_position(int timestep) const;
+    double get_time(int timestep) const;
+
+    Ogre::SceneNode *get_node() const;
+    void generate_anim();
 
 };
